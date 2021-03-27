2c72c8efd8ace0da19ea1f38e37d7f571ba4570f
Leticia Echarri <leticia.echarri@gmail.com>
2021-03-27 17:10:17 +0100

    feat: Necesary changes

--- games/common/protocols/NavigationBarProtocol.swift
+++ games/common/protocols/NavigationBarProtocol.swift
@@ -5,4 +5,23 @@
 //  Created by Leticia Personal on 27/03/2021.
 //
 
-import Foundation
+import UIKit
+
+protocol NavigationBarProtocol {
+    func navigationBarButtons()
+    func closeSession()
+}
+
+extension NavigationBarProtocol where Self: UIViewController {
+    
+    func navigationBarButtons() {
+        navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
+        
+        let exit = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: nil)
+        navigationItem.rightBarButtonItem = exit
+        
+        exit.actionClosure = {
+            self.closeSession()
+        }
+    }
+}
--- games/common/protocols/OpenTrivialDataBase.swift
+++ games/common/protocols/OpenTrivialDataBase.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 typealias QuestionsResponseBlock = ([QuestionModel]?, Error?) -> Void
+typealias SessionTokenResponseBlock = (String?, Error?) -> Void
 
 protocol OpenTrivialDataBase: DataSource {
     func requestQuestions(with model: OpenTrivialRequestModel, completion: @escaping QuestionsResponseBlock)
@@ -24,6 +25,8 @@ extension OpenTrivialDataBase {
             urlComponents.queryItems = model.getParameters()
             
             guard let url = urlComponents.url else {
+                let error = NSError(domain:"", code: 0, userInfo: [NSLocalizedDescriptionKey: "Questions error - url no valid"]) as Error
+                completion(nil, error)
                 return
             }
             
@@ -32,16 +35,29 @@ extension OpenTrivialDataBase {
                 if let data = response as? Data {
                     do {
                         let questions = try JSONDecoder().decode(QuestionsModel.self, from: data)
-                        let formatQuestions = questions.results.compactMap({ item in
-                            return QuestionModel(category: item.category, type: item.type, difficulty: item.difficulty, question: item.question.formatted(), answer: item.answer.formatted(), incorrectAnswers: format(item.incorrectAnswers))
-                        })
-                        completion(formatQuestions, nil)
+                        guard let responseCode = OpenTrivialResponseCode(rawValue: questions.responseCode) else {
+                            let error = NSError(domain:"", code: 1, userInfo: [NSLocalizedDescriptionKey: "Questions error - no data"])
+                            completion(nil, error)
+                            return
+                        }
+                        
+                        if responseCode == .success {
+                            let formatQuestions = questions.results.compactMap({ item in
+                                return QuestionModel(category: item.category, type: item.type, difficulty: item.difficulty, question: item.question.formatted(), answer: item.answer.formatted(), incorrectAnswers: format(item.incorrectAnswers))
+                            })
+                            completion(formatQuestions, nil)
+                            
+                        } else {
+                            let error = NSError(domain:"", code: responseCode.rawValue, userInfo: [NSLocalizedDescriptionKey: responseCode.getDescription()]) as Error
+                            completion(nil, error)
+                        }
+                        
                     } catch {
                         completion(nil, error)
                     }
                     
                 } else {
-                    let error = NSError(domain:"", code: 0, userInfo:[ NSLocalizedDescriptionKey: "Questions error - no data"]) as Error
+                    let error = NSError(domain:"", code: 0, userInfo: [NSLocalizedDescriptionKey: "Questions error - no data"]) as Error
                     completion(nil, error)
                 }
                 
@@ -54,6 +70,33 @@ extension OpenTrivialDataBase {
     
     private func format(_ questions: [String]) -> [String] {
         return questions.compactMap({ $0.formatted() })
+    }
+    
+    func retrieveSessionToken(completion: @escaping SessionTokenResponseBlock) {
+        guard let url = URL(string: "https://opentdb.com/api_token.php?command=request") else {
+            return
+        }
+        
+        executeRequest(url: url) { (response) in
+            
+            if let data = response as? Data {
+                
+                do {
+                    let questions = try JSONDecoder().decode(SessionTokenModel.self, from: data)
+                    completion(questions.token, nil)
+                } catch {
+                    completion(nil, error)
+                }
+                
+            } else {
+                let error = NSError(domain:"", code: 0, userInfo: [NSLocalizedDescriptionKey: "Request token error - no data"]) as Error
+                completion(nil, error)
+            }
+            
+        } failure: { (error) in
+            completion(nil, error)
+        }
+
     }
     
 }
@@ -78,5 +121,28 @@ struct OpenTrivialRequestModel {
             params.append(URLQueryItem(name: "token", value: token))
         }
         return params
+    }
+}
+
+enum OpenTrivialResponseCode: Int {
+    case success = 0
+    case noResults = 1
+    case invalidParameter = 2
+    case tokenNotFound = 3
+    case tokenEmpty = 4
+    
+    func getDescription() -> String {
+        switch self {
+        case .success:
+            return "Returned results successfully"
+        case .noResults:
+            return "Could not return results."
+        case .invalidParameter:
+            return "Contains an invalid parameter"
+        case .tokenNotFound:
+            return "Session Token does not exist"
+        case .tokenEmpty:
+            return "Session Token has returned all possible questions for the specified query. Resetting the Token is necessary."
+        }
     }
 }
